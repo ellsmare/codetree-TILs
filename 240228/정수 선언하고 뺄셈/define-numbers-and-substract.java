7267c9a711def007a848d85aeca844e15f66cedf
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-02-28 16:21:44 +0900

    Title: 정수 선언하고 뺄셈, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240228/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        //정수형 변수 2개 선언 97,13 대입
+        int a=97, b=13;
+        System.out.print(a+" - "+b+" = "+(a-b));
+        //System.out.print("%d-%d=%d"+a,+b,+(a-b));
+
+
+        //코딩 컨벤션 -CamelCase 방식
+
+    }
+}
