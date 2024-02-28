b9c65e578c1bf65879d5670982baface76856f9f
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-02-28 16:41:08 +0900

    Title: 변수 선언하기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240228/변수 선언하기/declaring-variables.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        // 변수 a,b 각각 3, 'C'
+        int a=3;
+        char b='C';
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
