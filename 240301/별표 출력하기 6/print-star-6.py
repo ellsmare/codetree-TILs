1d3345a5594b9542386468219034622125bcff53
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-03-01 23:52:24 +0900

    Title: 별표 출력하기 6, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240301/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,15 @@
+n=int(input());
+num=n-1;
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ");
+    for j in range(2*n-(2*i+1)):
+        print("*",end=" ");
+    print();
+
+for i in range(num):
+    for j in range(num-(i+1)):
+        print(" ",end=" ");
+    for j in range(num+(2*i)):
+        print("*",end=" ");
+    print();
