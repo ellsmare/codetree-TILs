8ad2eb24c71c8155230456d3cebd9628cc001e1e
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-03-01 23:57:32 +0900

    Title: 별표 출력하기 6, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240301/별표 출력하기 6/print-star-6.py
+++ 240301/별표 출력하기 6/print-star-6.py
@@ -10,6 +10,6 @@ for i in range(n):
 for i in range(num):
     for j in range(num-(i+1)):
         print(" ",end=" ");
-    for j in range(num+(2*i)):
+    for j in range(2*(i+1)+1):
         print("*",end=" ");
     print();
