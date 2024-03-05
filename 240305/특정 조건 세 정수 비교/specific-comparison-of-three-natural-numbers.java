5fa168ec30e8058540fa0a4c4e485429b0f46eda
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-03-05 14:41:41 +0900

    Title: 특정 조건 세 정수 비교, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240305/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240305/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -8,15 +8,15 @@ public class Main {
         b=sc.nextInt();
         c=sc.nextInt();
 
-        if(a<b && a<c){
+        if(a<=b && a<=c){
             System.out.print(1+" ");
         }else{
             System.out.print(0+" ");
         }
         if(a==b && b==c){
-            System.out.println(1);
+            System.out.print(1);
         }else{
-            System.out.println(0);
+            System.out.print(0);
         }
     
     }
