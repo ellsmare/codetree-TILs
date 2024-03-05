b0cb3401a6f218c94a6c5dd55e69fba14f3deb8e
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-03-05 12:26:08 +0900

    Title: 알파벳에 따른 평가, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String letter=sc.next();
+        char c = letter.charAt(0);
+        if(c=='S'){
+            System.out.println("Superior");
+        }
+        else if(c=='A'){
+            System.out.println("Excellent");
+        }
+        else if(c=='B'){
+            System.out.println("Good");
+        }
+        else if(c=='C'){
+            System.out.println("Usually");
+        }
+        else if(c=='D'){
+            System.out.println("Effort");
+        }
+        else {
+            System.out.println("Failure");
+        }
+
+    }
+}
