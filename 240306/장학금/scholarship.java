1ba0b176e8c20659b0b9a67b8623bbbd3fab9c11
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-03-06 19:32:21 +0900

    Title: 장학금, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240306/장학금/scholarship.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+        int middleScore = sc.nextInt();
+        int lastScore = sc.nextInt();
+        if(middleScore>=90 && lastScore >=95){
+            System.out.println(100000);
+        }else if(middleScore>=90 && lastScore >=95){
+            System.out.println(50000);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
