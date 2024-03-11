64bd64d9e43af347449c7d36c3cef03afd20cded
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-03-11 14:22:25 +0900

    Title: 증가시키며 출력하기, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240311/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for(int n = 5; n<=17; n+=2){
+            System.out.print(n+" ");
+        }
+    }
+}
