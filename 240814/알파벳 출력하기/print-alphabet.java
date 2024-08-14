3f968715aaf2eb948f344a8d4e4b2bda8772770a
ellsmare <131947450+ellsmare@users.noreply.github.com>
2024-08-14 15:26:00 +0900

    Title: 알파벳 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240814/알파벳 출력하기/print-alphabet.java
+++ 240814/알파벳 출력하기/print-alphabet.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // 삼각형 모양 1≤n≤15  65 'A'
+        // 삼각형 모양 1≤n≤15  65 'A' 90 'Z'
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int cnt=65;
@@ -10,6 +10,10 @@ public class Main {
             for (int j = 0; j <i+1; j++) {
                 System.out.print((char)cnt);   
                 cnt++;
+
+                if(cnt>90){
+                    cnt=65;
+                }
             }   
             System.out.println();   
         }
